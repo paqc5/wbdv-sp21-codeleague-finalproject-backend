6b139c1fc60b7105c8683cd6f52146bb5bd3dbaf
Mac User <macuser@MacintoshUsers-MacBook-2.local>
2021-04-04 21:45:59 -0600

    added findUserTeam to user-service and user-controller

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -2,8 +2,11 @@ const userService = require('../services/user-service');
 
 module.exports = (app) => {
   app.get('/api/user/team', function (req, res) {
-    userService.getTeam().then((team) => {
-      res.send(team);
-    });
+    // read email and password from request body
+    userService
+      .findUserTeam('hluzinho@gmail.com', 'codeLeague123')
+      .then((team) => {
+        res.send(team);
+      });
   });
 };
