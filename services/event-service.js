25c35cb4396c09d5d5047970444e11c5782dfd43
Paola Quevedo <quevedocontreras.p@husky.neu.edu>
2021-04-09 22:31:53 -0700

    Added findEventAndMatches function to event services

--- services/event-service.js
+++ services/event-service.js
@@ -1,26 +1,120 @@
 const axios = require('axios');
 const BASE_API_URL = 'https://fantasy.premierleague.com/api/bootstrap-static/';
+const FIXTURES_API_URL = 'https://fantasy.premierleague.com/api/fixtures'
 const PROXY_API_URL = 'https://codeleague-cors-proxy.herokuapp.com/';
 
-let getConfig = {
-    method: 'get',
-    url: PROXY_API_URL,
-    headers: { 'Target-URL': BASE_API_URL, 'Content-Type': 'application/json' },
+let baseConfig = {
+  url: PROXY_API_URL,
+  method: 'GET',
+  headers: {
+    'target-url': BASE_API_URL,
+    'content-type': 'application/json',
+    'Cache-Control': 'no-cache',
+    'Pragma': 'no-cache',
+    'Expires': '0',
+  }
 };
+
+let fixturesConfig = {
+  url: PROXY_API_URL,
+  method: 'GET',
+  headers: {
+    'target-url': FIXTURES_API_URL,
+    'content-type': 'application/json',
+    'Cache-Control': 'no-cache',
+    'Pragma': 'no-cache',
+    'Expires': '0',
+  }
+}
 
 const findAllEvents = () => {
-    return axios(getConfig)
-        .then((response) => response.data.events)
-        .catch((error) => {
-            console.log(error);
-        });
-};
+  return axios(baseConfig)
+    .then(response => response.data.events)
+    .catch((error) => {
+      console.log(error);
+    });
+}
 
 const findEventById = (eventId) =>
-    findAllEvents().then((res) =>
-        res.filter((event) => event.id === eventId)
-    );
+  findAllEvents().then((res) =>
+    res.filter((event) => event.id === eventId)
+  )
+
+const findCurrentEvent = () => {
+  return axios(baseConfig)
+    .then(response =>
+      response.data.events.filter(event => event.is_next === true))
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+// const findMatchesForEvent = (eventId) => {
+//   axios.get(PROXY_API_URL, fixturesConfig)
+//     .then(response => 
+//       response.data.filter(event => event.event === eventId))
+//         .catch((error) => {
+//           console.log(error);
+//         });
+// }
+
+const findEventAndMatches = () => {
+
+  let matches = {}
+
+  // Get Json object from statis API
+  return axios(baseConfig)
+    .then(baseRs => {
+
+      // Get the current event
+      let rs = baseRs.data.events.filter(event => event.is_next === true)
+
+      // Update matched object with the id and event name
+      matches['event_id'] = rs[0].id
+      matches['event_name'] = rs[0].name
+
+      // Get Json object from fixture API
+      return axios(fixturesConfig)
+        .then(fixRs => {
+
+          // Get all the matches that matches the event id
+          rs = fixRs.data.filter(event => event.event === matches.event_id)
+
+          // Put together Json object with event id, matches, and time
+          return matches = {
+            ...matches,
+            matches: rs.map(item => {
+
+              let teamH = baseRs.data.teams.filter(team =>
+                team.id === item.team_h)
+
+              let teamA = baseRs.data.teams.filter(team =>
+                team.id === item.team_a)
+
+              let tmpJson = {
+                match_id: item.id,
+                kickoff_time: item.kickoff_time,
+                team_h: teamH[0].name,
+                team_h_short: teamH[0].short_name,
+                team_h_img: teamH[0].code,
+                team_a: teamA[0].name,
+                team_a_short: teamA[0].short_name,
+                team_a_img: teamA[0].code,
+              }
+              return tmpJson
+            })
+          }
+        })
+
+        .catch((error) => {
+          console.log(error);
+        });
+
+    }).catch((error) => {
+      console.log(error);
+    })
+}
 
 module.exports = {
-    findAllEvents, findEventById
+  findAllEvents, findCurrentEvent, findEventById, findEventAndMatches
 }
--- services/player-service.js
+++ services/player-service.js
@@ -1,23 +1,38 @@
 // bring api data into service
 // manipulate it in service
-const fpl = require('fpl-api');
+// const fpl = require('fpl-api');
 const axios = require('axios');
 const BASE_API_URL = 'https://fantasy.premierleague.com/api/bootstrap-static/';
+const FIXTURES_API_URL = 'https://fantasy.premierleague.com/api/fixtures'
 const API_URL_HISTORY =
     'https://fantasy.premierleague.com/api/element-summary/251/';
 const PROXY_API_URL = 'https://codeleague-cors-proxy.herokuapp.com/';
-let players;
+
 // do one all api info call
 // cache
-let getConfig = {
-    method: 'get',
+let baseConfig = {
     url: PROXY_API_URL,
-    headers: { 'Target-URL': BASE_API_URL, 'Content-Type': 'application/json' },
+    method: 'GET',
+    headers: {
+        'target-url': BASE_API_URL,
+        'content-type': 'application/json',
+        'Cache-Control': 'no-cache',
+        'Pragma': 'no-cache',
+        'Expires': '0',
+    }
 };
 
-// variable that checks for null
-// if null fetches
-// if not
+let fixturesConfig = {
+    url: PROXY_API_URL,
+    method: 'GET',
+    headers: {
+        'target-url': FIXTURES_API_URL,
+        'content-type': 'application/json',
+        'Cache-Control': 'no-cache',
+        'Pragma': 'no-cache',
+        'Expires': '0',
+    }
+}
 
 const findAllPlayers = () => {
     // Alternative approach
@@ -26,12 +41,8 @@ const findAllPlayers = () => {
     // [{playerId:, name:, totalPoints..., team:}, {playerId:, name:, totalPoints...}]
     // a list of objects that is a hashmap where key is element code
 
-    return axios(getConfig)
-        .then((response) => {
-            players = response.data.elements
-            response.data.elements
-            console.log(players)
-        })
+    return axios(baseConfig)
+        .then((response) => response.data.elements)
         .catch((error) => {
             console.log(error);
         });
@@ -50,7 +61,7 @@ const findPlayer = (playerId) =>
     );
 
 const findPlayerPosition = (elementTypeId) => {
-    return axios(getConfig)
+    return axios(baseConfig)
         .then((res) => {
             return res.data.element_types;
         })
@@ -59,10 +70,24 @@ const findPlayerPosition = (elementTypeId) => {
         );
 };
 
+
+//TODO: Finish get all the games positions
+// const findPlayersPositions = () => {
+//     return axios(baseConfig)
+//         .then(response => {
+//             console.log("in service")
+//             console.log(response.data.element_types)
+//         })
+//         .catch((error) => {
+//             console.log(error);
+//         });
+// };
+
 module.exports = {
     findAllPlayers,
     findPlayer,
     findPlayerPosition,
+    findPlayersPositions
 };
 
 // findAllPlayers
