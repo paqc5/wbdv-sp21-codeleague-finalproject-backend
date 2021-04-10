d160c04cb077b7948cbc2bb60dd9ab8439c79676
Paola Quevedo <quevedocontreras.p@husky.neu.edu>
2021-04-10 01:15:01 -0700

    Added findPlayerByName function in player service

--- services/player-service.js
+++ services/player-service.js
@@ -5,47 +5,47 @@ const axios = require('axios');
 const BASE_API_URL = 'https://fantasy.premierleague.com/api/bootstrap-static/';
 const FIXTURES_API_URL = 'https://fantasy.premierleague.com/api/fixtures'
 const API_URL_HISTORY =
-    'https://fantasy.premierleague.com/api/element-summary/251/';
+  'https://fantasy.premierleague.com/api/element-summary/251/';
 const PROXY_API_URL = 'https://codeleague-cors-proxy.herokuapp.com/';
 
 // do one all api info call
 // cache
 let baseConfig = {
-    url: PROXY_API_URL,
-    method: 'GET',
-    headers: {
-        'target-url': BASE_API_URL,
-        'content-type': 'application/json',
-        'Cache-Control': 'no-cache',
-        'Pragma': 'no-cache',
-        'Expires': '0',
-    }
+  url: PROXY_API_URL,
+  method: 'GET',
+  headers: {
+    'target-url': BASE_API_URL,
+    'content-type': 'application/json',
+    'Cache-Control': 'no-cache',
+    'Pragma': 'no-cache',
+    'Expires': '0',
+  }
 };
 
 let fixturesConfig = {
-    url: PROXY_API_URL,
-    method: 'GET',
-    headers: {
-        'target-url': FIXTURES_API_URL,
-        'content-type': 'application/json',
-        'Cache-Control': 'no-cache',
-        'Pragma': 'no-cache',
-        'Expires': '0',
-    }
+  url: PROXY_API_URL,
+  method: 'GET',
+  headers: {
+    'target-url': FIXTURES_API_URL,
+    'content-type': 'application/json',
+    'Cache-Control': 'no-cache',
+    'Pragma': 'no-cache',
+    'Expires': '0',
+  }
 }
 
 const findAllPlayers = () => {
-    // Alternative approach
-    // get all players
-    // put in list with information we want
-    // [{playerId:, name:, totalPoints..., team:}, {playerId:, name:, totalPoints...}]
-    // a list of objects that is a hashmap where key is element code
+  // Alternative approach
+  // get all players
+  // put in list with information we want
+  // [{playerId:, name:, totalPoints..., team:}, {playerId:, name:, totalPoints...}]
+  // a list of objects that is a hashmap where key is element code
 
-    return axios(baseConfig)
-        .then((response) => response.data.elements)
-        .catch((error) => {
-            console.log(error);
-        });
+  return axios(baseConfig)
+    .then((response) => response.data.elements)
+    .catch((error) => {
+      console.log(error);
+    });
 };
 
 // how do you put promise response in a variable for caching
@@ -55,39 +55,39 @@ const findAllPlayers = () => {
 // can cache all players on the frontend or the backend
 // why do I have to return the axios call
 // axios-cache-adapter
-const findPlayer = (playerId) =>
-    findAllPlayers().then((res) =>
-        res.filter((player) => player.id === playerId)
-    );
+const findPlayerById = (playerId) =>
+  findAllPlayers().then((res) =>
+    res.filter((player) => player.id === playerId)
+  );
 
 const findPlayerPosition = (elementTypeId) => {
-    return axios(baseConfig)
-        .then((res) => {
-            return res.data.element_types;
-        })
-        .then((res) =>
-            res.filter((elementType) => elementType.id === elementTypeId)
-        );
+  return axios(baseConfig)
+    .then((res) => {
+      return res.data.element_types;
+    })
+    .then((res) =>
+      res.filter((elementType) => elementType.id === elementTypeId)
+    );
 };
 
-
-//TODO: Finish get all the games positions
-const findPlayersPositions = () => {
-    return axios(baseConfig)
-        .then(response => {
-            console.log("in service")
-            console.log(response.data.element_types)
-        })
-        .catch((error) => {
-            console.log(error);
-        });
-};
+const findPlayerByName = (inputNameOne, inputNameTwo) => {
+  return axios(baseConfig).then(res =>
+    res.data.elements.filter(player => {
+      let firstName = player.first_name !== undefined ? player.first_name.toLowerCase() : ""
+      let lastName = player.second_name !== undefined ? player.second_name.toLowerCase() : ""
+      if ((firstName === inputNameOne) || (firstName === inputNameTwo) || (lastName === inputNameOne) || (lastName === inputNameTwo)) {
+        return true
+      }
+      return false
+    })
+  )
+}
 
 module.exports = {
-    findAllPlayers,
-    findPlayer,
-    findPlayerPosition,
-    findPlayersPositions
+  findAllPlayers,
+  findPlayerById,
+  findPlayerByName,
+  findPlayerPosition,
 };
 
 // findAllPlayers
