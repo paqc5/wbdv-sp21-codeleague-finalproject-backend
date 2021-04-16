d048a9b509a8d7a56b76eace60f5156d49c45ffa
Paola Quevedo <quevedocontreras.p@husky.neu.edu>
2021-04-15 23:21:08 -0700

    Added default message for home path

--- server.js
+++ server.js
@@ -32,6 +32,34 @@ app.use(function (req, res, next) {
   next();
 });
 
+const apiExplanation = (req, res) => {
+  let result = `
+  <h3>Welcome to CodeLeague Api, <br/>you can use the following paths in order access the data:</h3>
+  
+  <h4>-- Players Info --</h4>
+  <p>/api/players</p>
+  <p>/api/players/:playerId</p>
+  <p>/api/search/players</p>
+  <p>/api/players/:playerId/details</p>
+  <p>/api/players/:playerId/positions/:elementTypeId</p>
+  <p>/api/search/players/top-ten</p>
+
+  <h4>-- Events Info --</h4>
+  <p>/api/events</p>
+  <p>/api/events/:eventId</p>
+  <p>/api/events/:eventId/matches</p>
+  <p>/api/matchweek</p>
+
+  <h4>-- Players' Positions Info --</h4>
+  <p>/api/types<p>
+  <p>/api/types/:typeId<p>
+  
+  `
+  res.send(result)
+};
+
+app.get('/', apiExplanation);
+
 require('./controllers/element-type-controller')(app);
 require('./controllers/event-controller')(app);
 require('./controllers/team-controller')(app);
