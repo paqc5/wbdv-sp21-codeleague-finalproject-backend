df0d759e03fb8f9d757db40f08058de8d10e75a5
Mac User <macuser@MacintoshUsers-MacBook-2.local>
2021-04-22 12:34:27 -0600

    coding login

--- controllers/users-team-controller.js
+++ controllers/users-team-controller.js
@@ -1,13 +1,22 @@
 const usersTeamService = require('../services/users-team-service');
 
 module.exports = (app) => {
-  
+  // test route
+  app.get('/api/user/cookie', function (req, res) {
+    // read email and password from request body
+    usersTeamService
+      .viewCookie('hluzinho@gmail.com', 'codeLeague123')
+      .then((cookie) => {
+        res.send(cookie);
+      });
+  });
+
   app.get('/api/user/team', function (req, res) {
     // read email and password from request body
-      usersTeamService
+    usersTeamService
       .findUserTeam('hluzinho@gmail.com', 'codeLeague123')
       .then((team) => {
         res.send(team);
       });
   });
-}
+};
--- services/users-team-service.js
+++ services/users-team-service.js
@@ -4,20 +4,33 @@ const playerService = require('../services/player-service');
 // implement authenticate user separately
 // will use to log users into the site
 
+// test method
+const viewCookie = (userEmail, userPassword) => {
+  return fpl
+    .fetchSession(userEmail, userPassword)
+    .then((cookie) => cookie)
+    .catch((error) => {
+      // authentication note
+      let authNote = { authenticated: false, api_msg: error.message };
+      console.log(error);
+      return authNote;
+    });
+};
 
 // TODO: import cached players from player service
 const findUserTeam = (userEmail, userPassword) => {
   //   console.log('players:', playerService.players);
   return fpl
     .fetchSession(userEmail, userPassword)
-    .then((cookie) =>
-      fpl.fetchCurrentUser(cookie).then((user) =>
+    .then((cookie) => {
+      console.log('cookie:', cookie);
+      return fpl.fetchCurrentUser(cookie).then((user) =>
         // const user = res;
         fpl.fetchMyTeam(cookie, user.player.entry)
-      )
-    )
+      );
+    })
     .then((team) =>
-      playerService.findAndParseAllPlayers().then((allPlayers) => {
+      playerService.findAllPlayers().then((allPlayers) => {
         const userTeam = team.picks.map((player) =>
           allPlayers.filter(
             (singlePlayer) => singlePlayer.id === player.element
@@ -38,4 +51,5 @@ function getUserTeam(fplEmail, fplPassword, managerId) {
 module.exports = {
   getUserTeam: getUserTeam,
   findUserTeam: findUserTeam,
+  viewCookie: viewCookie,
 };
